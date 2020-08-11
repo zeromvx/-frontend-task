25a6dc3e576e6ee780315d0448054de40481dbf0
Sergey Karmanov <zeromvx@gmail.com>
2020-08-11 09:07:18 +0300

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header className="header">
+    <img src="" alt="logo" className="header__logo"/>
+    <h1 className="header__title">Чистая вода</h1>
+    <p className="header__descr"></p>
+  </header>
+)
+
+export default Header;
