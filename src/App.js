d85bc3cdd2d8cc0301b145cff815e3ef4f9180bf
Sergey Karmanov <zeromvx@gmail.com>
2020-08-11 09:22:15 +0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import "./header.scss"
+
 const Header = () => (
   <header className="header">
-    <img src="" alt="logo" className="header__logo"/>
+    <img src={'./logo.svg'} alt="logo" className="header__logo"/>
     <h1 className="header__title">Чистая вода</h1>
-    <p className="header__descr"></p>
+    <p className="header__descr">
+      Питьевая вода, предназначенная для ежедневного потребления, идеально подходит для кулеров. Закажите доставку воды и мы привезем её на дом или в офис.
+    </p>
   </header>
 )
 
